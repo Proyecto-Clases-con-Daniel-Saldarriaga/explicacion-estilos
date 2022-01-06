2d6996a65d9792ad107a9537f2483ef12491bca5
ismaelc511 <ismaelcarvajal@outlook.com>
2022-01-06 10:50:33 -0500

    Agregar explicación de talwind segunda parte y de responsive

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col md:flex-row">
       <div className="mx-2 bg-green-500">Elemento 1</div>
       <div className="mx-2 bg-red-800">Elemento 2</div>
       <div className="mx-2 bg-purple-400">Elemento 3</div>
