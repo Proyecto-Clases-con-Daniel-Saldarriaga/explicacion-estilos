224275caf16d1aa91caf262157f0789b8e69b3a4
ismaelc511 <ismaelcarvajal@outlook.com>
2022-01-06 13:48:13 -0500

    Agrego cambio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
      <div className="fondo1">Este es un fondo negro</div>
      <div className="fondo2">Este es un fondo negro</div>
-     <div className="fondo3">Este es un fondo negro</div>
+     <div className="fondo1">Este es un fondo negro</div>
     </div>
   );
 }
