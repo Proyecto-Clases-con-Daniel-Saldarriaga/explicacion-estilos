2ee0aeb110c5f616f90dd57290b50c4665ff29a9
ismaelc511 <ismaelcarvajal@outlook.com>
2022-01-06 10:26:22 -0500

    Explicación de talwind primera parte

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex flex-col items-center justify-center">
+      <div className="mx-2 bg-green-500">Elemento 1</div>
+      <div className="mx-2 bg-red-800">Elemento 2</div>
+      <div className="mx-2 bg-purple-400">Elemento 3</div>
+      <div className="mx-2 bg-yellow-500">Elemento 4</div>
+      <buttom className="boton">Haz click aquí</buttom>
+      <buttom className="boton">Este es el boton 2</buttom>
+    </div>
     </div>
   );
 }
+
 
 export default App;
